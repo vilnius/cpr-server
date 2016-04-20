7b42e6eacd8ef1bc201f4ebfdf6d08c8b3679f45
Matas Seimys <matas.seimys@gmail.com>
2016-04-20 23:59:40 +0300

    Added script when we need to update frontend/backend

---
+++ build-cpr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# A script for building/updating frontend and backend
+#
+# ROOT = directory of this bash file
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#
+# CPR Frontend
+cd $ROOT/cpr-frontend
+echo "- Updating frontend source code..."
+git pull
+echo "- Installing npm dependencies..."
+npm install
+echo "- Building frontend..."
+npm run prebuild:prod
+npm run build:prod
+#
+# CPR Server
+cd $ROOT/cpr-server
+echo "- Updating backend source code..."
+git pull
+echo "- Installing npm dependencies..."
+npm install
+echo "- (Re)starting server..."
+npm start  # here must not suck
