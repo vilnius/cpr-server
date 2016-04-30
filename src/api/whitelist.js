0ac8e9576ee368d21c18332a8657151ba4b84a93
kodvin <paulauskas_andrius@yahoo.com>
2016-04-30 19:02:58 +0300

    add all the necessary backend  code for white palte adding and removing

---
+++ src/api/whitelist.js
@@ -0,0 +1,73 @@
+import { Router } from 'express';
+
+import { isAuthenticated } from '../helpers';
+import { WhitePlate } from '../models';
+
+export default function() {
+  var api = Router();
+
+  api.get('/', isAuthenticated, (req, res) => {
+    WhitePlate.find({}, (err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+
+  api.post('/', isAuthenticated, (req, res) => {
+    var whiteplate = new WhitePlate(req.body);
+
+    whiteplate.save((err, data) => {
+      if (err) {
+        return res.status(400).json({ error: err.toString() });
+      }
+      res.status(201).json(data)
+    });
+  });
+
+  api.get('/:id', isAuthenticated, (req, res) => {
+    var id = req.params.id;
+
+    WhitePlate.findById(id, (err, data) => {
+      if (err) {
+        return res.status(400).json({ error: err.toString() });
+      }
+      if (!data) {
+        return res.status(404).json({status: 404, message: `Not found: ${id}`});
+      }
+      res.json(data)
+    });
+
+  });
+
+  api.post('/:id', isAuthenticated, (req, res) => {
+    var id = req.params.id;
+
+    WhitePlate.findByIdAndUpdate(id, req.body, { runValidators: true, new: true }, (err, data) => {
+      if (err) {
+        return res.status(400).json({ error: err.toString() });
+      }
+      if (!data) {
+        return res.status(404).json({status: 404, message: `Not found: ${id}`});
+      }
+      res.json(data)
+    });
+
+  });
+
+  api.delete('/:id', isAuthenticated, (req, res) => {
+    var id = req.params.id;
+
+    WhitePlate.findByIdAndRemove(id, (err, data) => {
+      if (err) {
+        return res.status(400).json({ error: err.toString() });
+      }
+      if (!data) {
+        return res.status(404).json({status: 404, message: `Not found: ${id}`});
+      }
+      res.json(data)
+    });
+
+  });
+
+  return api;
+}
---
+++ src/models/whiteplate.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+var WhitePlate = new Schema({
+  plate: String,
+  description: String,
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('WhitePlate', WhitePlate);
