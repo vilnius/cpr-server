bbd14a9c54af761d0546ac9798b1384a09c62caa
kodvin <paulauskas_andrius@yahoo.com>
2016-05-23 20:19:08 +0300

    add server side of whiteplate importing

--- src/api/whitelist.js
+++ src/api/whitelist.js
@@ -2,14 +2,45 @@ import { Router } from 'express';
 
 import { isAuthenticated } from '../helpers';
 import { WhitePlate } from '../models';
+import fs from 'fs';
+import busboy from 'connect-busboy';
+import xlsx from 'xlsx';
 
 export default function() {
   var api = Router();
+  api.use(busboy());
 
   api.get('/', isAuthenticated, (req, res) => {
     WhitePlate.find({}, (err, data) => {
       if (err) throw err;
       res.json(data);
+    });
+  });
+
+  api.post('/upload', isAuthenticated, (req, res) => {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+          var workbook = xlsx.readFile(__dirname + '/files/' + filename);
+          var worksheet = workbook.Sheets[workbook.SheetNames[0]];
+          //get number of rows
+          var range = xlsx.utils.decode_range(worksheet['!ref']);
+          var rows = range.e.r;
+          //starts at 1 and the first row is header hence i = 2;
+          //provided file has this structure: 
+          //A - garage Nr, B - automobile description, C - plane number
+          for (var i = 2; i <= rows; i++) {
+            saveWhitePlate({
+              plate: worksheet["C"+i].v,
+              description: worksheet["B"+i].v,
+              garageNr: worksheet["A"+i].v
+            });
+          }     
+          res.json("OK")
+        });
     });
   });
 
@@ -84,6 +115,16 @@ export default function() {
     });
 
   });
+
+  function saveWhitePlate(whitePlate) {
+    var whiteplate = new WhitePlate(whitePlate);
+
+    whiteplate.save((err, data) => {
+      if (err) {
+        //handle err
+      }
+    });
+  }
 
   return api;
 }
